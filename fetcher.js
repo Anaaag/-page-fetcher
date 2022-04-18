12072e76b327d3b16e6dde9404793236ba99eda2
Anaaag <agarc20@outlook.com>
2022-04-18 01:04:41 -0700

    completed

--- fetcher.js
+++ fetcher.js
@@ -5,5 +5,40 @@
 
 const request = require('request')
 const fs = require('fs')
-const readline = require('readline')
+
+
+const arguments = process.argv.slice(2)
+const url = arguments[0]
+const file = arguments[1]
+
+
+request(url, (error, response, body) => {
+
+  if (error) {
+    console.log("Error")
+  }
+
+  fs.writeFile('file', body, (err) => {
+    
+    if (err)  {
+      console.log('Cannot save file')
+
+    } else {
+      console.log(`Downloaded and saved ${response.headers["content-length"]} byes to ${file} `)
+    }
+    
+  }) 
+  
+
+})
+
+
+
+
+
+
+
+
+
+
 
