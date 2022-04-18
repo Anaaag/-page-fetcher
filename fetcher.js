b8a5768c601cbcfee68b5e78b7dc43a25bbff749
Anaaag <agarc20@outlook.com>
2022-04-17 23:17:15 -0700

    first steps

---
+++ fetcher.js
@@ -0,0 +1,9 @@
+// figure out the command line arguments - process.argv
+// make an http request with url -console.log
+// wait for a response 
+// receive data and write it to a file
+
+const request = require('request')
+const fs = require('fs')
+const readline = require('readline')
+
